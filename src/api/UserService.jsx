d23e78433ee381708f0a45c0b9b0bab6d7d43c34
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2023-09-16 23:22:40 +0300

    finished user controller endpoints

--- src/api/UserService.jsx
+++ src/api/UserService.jsx
@@ -1,18 +1,113 @@
-import ENDPOINTS from "../constants/ENDPOINTS";
+import { USER_ENDPOINTS } from '../constants/ENDPOINTS';
 
 const UsersService = {
-	async authenticate(jwtRequest) {
-		return await fetch(ENDPOINTS.AUTHENTICATE, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(jwtRequest),
-		}).then(async (response) => {
-			const jwtToken = await response.json();
-			return jwtToken;
-		});
-	},
-};
+  async authenticate(jwtRequest) {
+    return await fetch(USER_ENDPOINTS.AUTHENTICATE, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(jwtRequest)
+    }).then(async (response) => {
+      const jwtToken = await response.json();
+      return jwtToken;
+    });
+  },
 
+  async getUserWithSector(userId) {
+    return await fetch(`${USER_ENDPOINTS.GET_USER_WITH_SECTOR}/${userId}`, {
+      method: 'GET'
+    }).then(async (response) => {
+      const responseTemplateVO = await response.json();
+      return responseTemplateVO;
+    });
+  },
+
+  async getUser(userId) {
+    return await fetch(`${USER_ENDPOINTS.GET_USER}/${userId}`, {
+      method: 'GET'
+    }).then(async (response) => {
+      const user = await response.json();
+      return user;
+    });
+  },
+
+  async getUserByUsername(username) {
+    const requestParams = new URLSearchParams({
+      username: username
+    });
+
+    return await fetch(
+      `${USER_ENDPOINTS.GET_USER_BY_USERNAME}?${requestParams}`,
+      {
+        method: 'GET'
+      }
+    ).then(async (response) => {
+      const user = await response.json();
+      return user;
+    });
+  },
+
+  async setWorkVisibleForUser(userId, visible) {
+    const requestParams = new URLSearchParams({
+      visible: visible
+    });
+
+    return await fetch(
+      `${USER_ENDPOINTS.SET_WORK_VISIBLE_FOR_USER}/${userId}?${requestParams}`,
+      {
+        method: 'PUT'
+      }
+    ).then(async (response) => {
+      const user = await response.json();
+      return user;
+    });
+  },
+
+  async getAllUsers() {
+    return await fetch(`${USER_ENDPOINTS.GET_ALL_USERS}`, {
+      method: 'GET'
+    }).then(async (response) => {
+      const users = await response.json();
+      return users;
+    });
+  },
+
+  async getAllUsersInSector(sectorId) {
+    return await fetch(
+      `${USER_ENDPOINTS.GET_ALL_USERS_IN_SECTOR}/${sectorId}`,
+      {
+        method: 'GET'
+      }
+    ).then(async (response) => {
+      const usersInSector = await response.json();
+      return usersInSector;
+    });
+  },
+
+  async getAllUsersInCompany(companyId) {
+    return await fetch(
+      `${USER_ENDPOINTS.GET_ALL_USERS_IN_COMPANY}/${companyId}`,
+      {
+        method: 'GET'
+      }
+    ).then(async (response) => {
+      const usersInCompany = await response.json();
+      return usersInCompany;
+    });
+  },
+
+  async saveUser(newUser) {
+    return await fetch(`${USER_ENDPOINTS.SAVE_USER}}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    }).then(async (response) => {
+      const user = await response.json();
+      return user;
+    });
+  }
+};
 export default UsersService;
--- src/constants/ENDPOINTS.jsx
+++ src/constants/ENDPOINTS.jsx
@@ -1,21 +1,23 @@
-let BASE_URL = "http://localhost:7171";
+let BASE_URL = 'http://localhost:7171';
 
 switch (process.env.NODE_ENV) {
-	case "development":
-		BASE_URL = "http://localhost:7171";
-		break;
+  case 'development':
+    BASE_URL = 'http://localhost:7171';
+    break;
 
-	default:
-		break;
+  default:
+    break;
 }
 
-const USER_ENDPOINTS = {
-	AUTHENTICATE: "authenticate",
+const USER_BASE_PATH = 'users';
+export const USER_ENDPOINTS = {
+  AUTHENTICATE: `${BASE_URL}/${USER_BASE_PATH}/authenticate`,
+  GET_USER: `${BASE_URL}/${USER_BASE_PATH}/simpleUser`,
+  GET_USER_BY_USERNAME: `${BASE_URL}/${USER_BASE_PATH}/user`,
+  GET_USER_WITH_SECTOR: `${BASE_URL}/${USER_BASE_PATH}`,
+  GET_ALL_USERS: `${BASE_URL}/${USER_BASE_PATH}/all`,
+  GET_ALL_USERS_IN_SECTOR: `${BASE_URL}/${USER_BASE_PATH}/all/sector`,
+  GET_ALL_USERS_IN_COMPANY: `${BASE_URL}/${USER_BASE_PATH}/all/company`,
+  SET_WORK_VISIBLE_FOR_USER: `${BASE_URL}/${USER_BASE_PATH}/setWorkVisible`,
+  SAVE_USER: `${BASE_URL}/${USER_BASE_PATH}/save`
 };
-
-const ENDPOINTS = {
-	//USER
-	AUTHENTICATE: `${BASE_URL}/${USER_ENDPOINTS.AUTHENTICATE}`,
-};
-
-export default ENDPOINTS;
